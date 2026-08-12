ae420d510bc4fef536a2a8b403191e7697ffd787
YeliXon <yeisonaguerram@gmail.com>
2026-08-11 20:47:25 -0500

    Creacion de la clase objetual

---
+++ ObjMatriz.java
@@ -0,0 +1,20 @@
+public class ObjMatriz {
+    int dato;
+
+    public ObjMatriz(int dato) {
+        this.dato = dato;
+    }
+
+    public ObjMatriz() {
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    
+}
